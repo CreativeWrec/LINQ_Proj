4af1dbd65228b9e6795137a15434c00f21e08f25
Kevin Harvey Jr <lordty@gmail.com>
2020-09-14 09:26:14 -0500

    Finished_Linq_problems

---
+++ LINQ_Problem_Proj/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Problem_Proj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
+
+            var results = words.Where(w => w.Contains("th"));
+            foreach (var word in results)
+            {
+                Console.WriteLine(results);
+            }
+
+            List<string> names = new List<string>() { "mike", "brad", "nevin", "zack", "mike" };
+
+            return names.Distinct().ToList();
+
+            public static double AverageGrades(List<string> gradeStrings)
+            {
+                return gradeStrings
+                    .Select(s => s.Split(',')
+                        .Select(g => Double.Parse(g))
+                            .OrderBy(a => a).Skip(1).Average())
+                    .Average();
+            }
+
+        }
+    }
+}
